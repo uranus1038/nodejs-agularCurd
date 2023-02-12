475e034c6ade141dde713367673b470f6fd58fa6
you <anusorn1039@gmail.com>
2023-02-12 21:36:14 +0700

    shared component angular

--- Frontend-app/src/app/app.component.ts
+++ Frontend-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Com1Component } from './com/com1/com1.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend-app';
+  messageChild:any ; 
+  // ViewChild
+  //@ViewChild("com1")
+  //com1:Com1Component = new Com1Component;
+  public getColor():void
+  {
+   //this.com1.c += 20 ;  
+  }
+  public chang(event:any)
+  {
+    this.messageChild = event ; 
+  }
 }
