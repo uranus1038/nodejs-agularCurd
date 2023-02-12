15670dbd4d02e6f52aadbfa6f5ce6e6195f5e0c6
you <anusorn1039@gmail.com>
2023-02-13 03:43:59 +0700

    curd angular

--- Frontend-app/src/app/app-routing.module.ts
+++ Frontend-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Com1Component } from './com/com1/com1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ component:Com1Component,path:'com1'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
